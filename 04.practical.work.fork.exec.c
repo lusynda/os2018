310a3ecb74692528a2c99c476285db7a3c6b6d27
lusynda <nntrung97@gmail.com>
2018-01-13 12:39:56 +0700

    prac 4 submit version 2

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -2,21 +2,20 @@
 #include <unistd.h>
 
 int main(){
-	printf("main before fork()\n");
-	int pid = fork();
-	if(pid == 0){
-		int pid1 = fork();
-               	if(pid1 == 0){
-                        printf("i am a child after fork(), launching free -h\n");
-                        char *args1[]  = { "free", "-h", NULL};
-                        execvp("free", args1);
-                }
-                else{
-			printf("i am a child after fork(), launching ps -ef\n");
-                	char *args[]  = { "/bin/ps", "-ef", NULL};			
-                	execvp("/bin/ps", args);
-		}
-	}
-	return 0;
+        int pid = fork();
+        if(pid == 0){
+            printf("i am a child after fork(), launching ps -ef\n");
+            char *args[]  = { "/bin/ps", "-ef", NULL};      
+            execvp("/bin/ps", args);
+        }else printf("I am parent after fork(), child ps is %d\n", pid);
+		
+	int pid1 = fork();
+	if(pid1 == 0){
+		printf("i am a child after fork(), launching free -h\n");
+        	char *args[]  = { "free", "-h", NULL};      
+            	execvp("free", args);
+	}else printf("I am parent after fork(), child free is %d\n", pid1);
+        return 0;
 
 }
+
